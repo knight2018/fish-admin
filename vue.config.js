7b844d7629d443fd98ae81c719e118071471d691
knight2018 <34180683+knight2018@users.noreply.github.com>
2018-12-14 18:30:45 +0800

    Add files via upload

--- vue.config.js
+++ vue.config.js
@@ -11,9 +11,10 @@ const resolve = dir => {
 // 如果您的应用程序部署在子路径中，则需要在这指定子路径
 // 例如：https://www.foobar.com/my-app/
 // 需要将它改为'/my-app/'
+// iview-admin线上演示打包路径： https://file.iviewui.com/admin-dist/
 const BASE_URL = process.env.NODE_ENV === 'production'
-  ? '/'
-  : '/'
+  ? '/stat/'
+  : ''
 
 module.exports = {
   // Project deployment base
@@ -27,23 +28,19 @@ module.exports = {
   // tweak internal webpack configuration.
   // see https://github.com/vuejs/vue-cli/blob/dev/docs/webpack.md
   // 如果你不需要使用eslint，把lintOnSave设为false即可
-  lintOnSave: true,
-  // chainWebpack: config => {
-  //   config.resolve.alias
-  //     .set('@', resolve('src')) // key,value自行定义，比如.set('@@', resolve('src/components'))
-  //     .set('_c', resolve('src/components'))
-  // },
+  lintOnSave: false,
+  chainWebpack: config => {
+    config.resolve.alias
+      .set('@', resolve('src')) // key,value自行定义，比如.set('@@', resolve('src/components'))
+      .set('_c', resolve('src/components'))
+  },
   // 打包时不生成.map文件
   productionSourceMap: false,
   // 这里写你调用接口的基础路径，来解决跨域，如果设置了代理，那你本地开发环境的axios的baseUrl要写为 '' ，即空字符串
-   devServer: {
-     proxy: {
-       '/api': {
-         target: '192.168.1.194:8080/',
-         pathRewrite: {'^/api' : '/api'},
-         changeOrigin: true
-       }
-     }
-     
-   }
+  // devServer: {
+  //   proxy: 'http://fish.zhonglehd.cn/statis/'
+  // }4
+  devServer: {
+    proxy: 'http://fish-test.zhonglehd.cn/statis/'
+  }
 }
